33d751e1ff75842afa2bfd7c5c5d6ebddfe59502
Merryn <merrynhr@gmail.com>
2021-08-11 23:01:44 +0100

    added tutors controller

---
+++ app/controllers/api/v1/tutors_controller.rb
@@ -0,0 +1,59 @@
+module Api
+  module V1
+    class TutorsController < ApplicationController
+      def index
+        tutors = Tutor.all
+
+        render json: TutorSerializer.new(tutors, options).serialized_json
+      end
+
+      def show 
+        tutor = Tutor.find_by(slug: params[:slug])
+
+        render json: TutorSerializer.new(tutor, options).serialized_json
+      end
+
+      def create
+        tutor = Tutor.new(tutor_params)
+
+        if tutor.save
+          render json: TutorSerializer.new(tutor).serialized_json
+        else    
+          render json: { error: tutor.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        tutor = Tutor.find_by(slug: params[:slug])
+
+        if tutor.update(tutor_params)
+          render json: TutorSerializer.new(tutor, options).serialized_json
+        else    
+          render json: { error: tutor.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        tutor = Tutor.find_by(slug: params[:slug])
+
+        if tutor.destroy
+          head :no_content
+        else    
+          render json: { error: tutor.errors.messages }, status: 422
+        end
+      end
+
+      private 
+
+      def tutor_params
+        params.require(:tutor).permit(:name, :image_url)
+      end
+
+      def options
+        #provide options for adding to Tutor
+        @options ||= { include: %i[reviews]}
+      end
+
+    end
+  end
+end
