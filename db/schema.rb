84e28df4c20aeded7aced467de070751f1caa771
Merryn <merrynhr@gmail.com>
2021-08-13 18:26:28 +0100

    subjected added to tutors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_134452) do
+ActiveRecord::Schema.define(version: 2021_08_13_171343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 2021_08_12_134452) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "bio"
+    t.string "subject"
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,7 +40,5 @@ ActiveRecord::Schema.define(version: 2021_08_12_134452) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
-
-  add_foreign_key "reviews", "tutors"
 
 end
