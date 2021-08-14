e73fcf72a98320343db2b6103652532451a45bf3
Merryn <merrynhr@gmail.com>
2021-08-14 13:46:01 +0100

    duplicated subject seed deleted from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ tutors = Tutor.create([
     subject: "Painting",
     image_url: "https://www.bobross.com/content/bob_ross_img.png",
     bio: "let's paint some trees",
-    subject: "Art",
     email: "Bob@email.com"
   },
   {
@@ -20,7 +19,6 @@ tutors = Tutor.create([
     subject: "Business",
     image_url: "https://www.deborahmeaden.com/images/deborah_cutout.png",
     bio: "good stuff will happen if you learn with me",
-    subject: "Business",
     email: "Debs@email.com"
   },
   {
@@ -28,7 +26,6 @@ tutors = Tutor.create([
     subject: "conservation",
     image_url: "https://attenboroughfilm.com/wp-content/uploads/2020/09/image-da-clean-poster@2x.jpg",
     bio: "learn to build a better future",
-    subject: "conservation",
     email: "Dave@email.com"
   },
 
@@ -37,7 +34,6 @@ tutors = Tutor.create([
     subject: "Maths",
     image_url: "https://attenboroughfilm.com/wp-content/uploads/2020/09/image-da-clean-poster@2x.jpg",
     bio: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    subject: "Maths",
     email: "4@email.com"
   },
   {
@@ -45,7 +41,6 @@ tutors = Tutor.create([
     subject: "English",
     image_url: "https://attenboroughfilm.com/wp-content/uploads/2020/09/image-da-clean-poster@2x.jpg",
     bio: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    subject: "Programming",
     email: "5@email.com"
   },
   {
@@ -53,7 +48,6 @@ tutors = Tutor.create([
     subject: "History",
     image_url: "https://attenboroughfilm.com/wp-content/uploads/2020/09/image-da-clean-poster@2x.jpg",
     bio: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    subject: "English",
     email: "6@email.com"
   }
 ])
