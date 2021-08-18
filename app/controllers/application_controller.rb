db7d7404ded7a97f8bdce9524411590d28752729
Merryn <merrynhr@gmail.com>
2021-08-18 11:45:22 +0100

    subject and teacher bug sorted out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,12 @@ class ApplicationController < ActionController::Base
   before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user
+  skip_before_action :verify_authenticity_token
   
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :teacher, :subject])
   end
 
   def authenticate_user
